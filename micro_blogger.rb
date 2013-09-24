ac8767a5d6a731f8a814f7f90a756c7660868b53
Sarah Pitts <mccallsmedia@gmail.com>
2013-09-24 10:14:59 -0500

    adds an improved interface

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -15,7 +15,26 @@ class MicroBlogger
    		puts "Must be 140 characters or less"
   	end
   end
+
+  def run
+    command = ""
+    while command != "q"
+      puts ""
+      printf "enter command: "
+      input = gets.chomp
+      parts = input.split
+      command = parts[0]
+      case command
+         when 'q' then puts "Goodbye!"
+         when 't' then tweet(parts[1..-1].join(" "))
+         else
+           puts "Sorry, I don't know how to (#{command})"
+      end
+    end
+  end
+
+
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("MicroBlogger 140 test")
+blogger.run
